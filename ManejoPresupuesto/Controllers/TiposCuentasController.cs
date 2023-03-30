fbfccd4cca2e6c9fe1a14c2b17006bbb7f952dfc
Jacob Sanchez <jacob_ico@outlook.com>
2023-03-30 10:02:54 -0600

    Message error added when form is empty

--- ManejoPresupuesto/Controllers/TiposCuentasController.cs
+++ ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -1,11 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ManejoPresupuesto.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ManejoPresupuesto.Controllers
 {
     public class TiposCuentasController : Controller
     {
-        public IActionResult crear()
+        public IActionResult Crear()
         {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Crear(TipoCuenta tipoCuenta)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(tipoCuenta);
+            }
+
+
+
             return View();
         }
     }

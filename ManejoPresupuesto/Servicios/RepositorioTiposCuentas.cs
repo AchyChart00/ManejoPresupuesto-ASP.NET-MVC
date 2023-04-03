839c4f9212d9fe105ec9ee51666b1abd7673d9d1
Jacob Sanchez <jacob_ico@outlook.com>
2023-04-03 10:40:38 -0600

    controller validation added

--- ManejoPresupuesto/Servicios/RepositorioTiposCuentas.cs
+++ ManejoPresupuesto/Servicios/RepositorioTiposCuentas.cs
@@ -17,10 +17,23 @@ namespace ManejoPresupuesto.Servicios
         {
             using var connection = new SqlConnection(connectionString);
             var id = await connection.QuerySingleAsync<int>(
-                $@"INSERT INTO TiposCuentas(Nombre, UsuarioId, Orden)
+                @"INSERT INTO TiposCuentas(Nombre, UsuarioId, Orden)
                    Values(@Nombre, @UsuarioId, 0);
                    SELECT SCOPE_IDENTITY();", tipoCuenta);
             tipoCuenta.Id = id;
+        }
+
+        public async Task<bool> Existe(string nombre, int usuarioId)
+        {
+            using var connection = new SqlConnection(connectionString);
+            var existe = await connection.QueryFirstOrDefaultAsync<int>(
+                @"SELECT 1 
+                FROM TiposCuentas
+                WHERE Nombre = @Nombre AND UsuarioId = @UsuarioId;",
+                new { nombre, usuarioId}
+                );
+
+            return existe == 1;
         }
     }
 }

1f1dab16cb2c255b9ba893c0fc258ccc7ec61567
Jacob Sanchez <jacob_ico@outlook.com>
2023-04-12 14:57:17 -0600

    movimiento de cuentas added

--- ManejoPresupuesto/Servicios/Interfaces/IRepositorioTransacciones.cs
+++ ManejoPresupuesto/Servicios/Interfaces/IRepositorioTransacciones.cs
@@ -7,6 +7,7 @@ namespace ManejoPresupuesto.Servicios.Interfaces
         Task Actualizar(Transaccion transaccion, decimal montoAnterior, int cuentaAnterior);
         Task Borrar(int id);
         Task Crear(Transaccion transaccion);
+        Task<IEnumerable<Transaccion>> ObtenerPorCuentaId(ObtenerTransaccionesPorCuenta modelo);
         Task<Transaccion> ObtenerPorId(int id, int usuarioId);
     }
 }

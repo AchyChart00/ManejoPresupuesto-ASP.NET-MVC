3701b549b0cbcf0d323f0cb6ed9c65bd7962748c
Jacob Sanchez <jacob_ico@outlook.com>
2023-04-14 12:02:22 -0600

    submenu added

---
+++ ManejoPresupuesto/Servicios/Interfaces/IServicioReportes.cs
@@ -0,0 +1,10 @@
+﻿using ManejoPresupuesto.Models;
+
+namespace ManejoPresupuesto.Servicios.Interfaces
+{
+    public interface IServicioReportes
+    {
+        Task<ReporteTransaccionesDetalladas> ObtenerReporteTransaccionesDetalladas(int usuarioId, int mes, int anio, dynamic ViewBag);
+        Task<ReporteTransaccionesDetalladas> ObtenerReporteTransaccionesDetalladasPorCuenta(int usuarioId, int cuentaId, int mes, int anio, dynamic ViewBag);
+    }
+}
